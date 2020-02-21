fe96356f34f03ae2083f3c9a5637d71ae1994b5a
vinirangel <vinirangel@yahoo.com>
2020-02-21 14:19:17 -0300

    We did it boys

--- combinador.java
+++ combinador.java
@@ -1,38 +1,48 @@
 public class combinador
 {
-    public static boolean isFim(String s){
-        return (s.length() >= 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
-    }
+	public static boolean isFim(String s){
+		return (s.length() >= 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+	}
 
-    public static void combinar(String linha)
-    {
-        String[] temp = linha.split(" ");
-        String str1 = temp[0];
-        String str2 = temp[1];
-        String[] resp = new String[100];
-        int x = 0;
+	public static void combinar(String linha)
+	{
+		String[] temp = linha.split(" ");
+		String str1 = temp[0];
+		String str2 = temp[1];
+		String resp = "";
 
-        while(x < linha.length()-1)
-        {
-            //if (str1.charAt(x) != null && str2.charAt(x) != null)
-            if ( str1.compareTo(str2) <  0)
-            {
-                for ( int y = 0; y < str1.length(); y++)
-                {
-                    resp[y] = str1.charAt(y);
-                    str2.charAt(y);
-                }
-            }
-            x++;
-        }
-    }
-    public static void main(String[] args) {
-        String[] linha = new String[100];
-        int numEntrada = 0;
+		int x = 0;
+		for(x < str1.length() && x < str2.length(); x++)
+		{
+			resp += str1.charAt(x);
+			resp += str2.charAt(x);
+		}
 
-        do {
-            linha[numEntrada] = MyIO.readLine();
-        } while (isFim(linha[numEntrada++]) == false);
-        numEntrada--;
-    }
+		while ( x < str1.length() )
+		{
+			resp += str1.charAt(x);
+			x++;
+		}
+
+		while ( x < str2.length() )
+		{
+			resp += str2.charAt(x);
+			x++;
+		}
+
+		MyIO.println(resp);
+	}
+	public static void main(String[] args) {
+		String[] linha = new String[100];
+		int numEntrada = 0;
+
+		do {
+			linha[numEntrada] = MyIO.readLine();
+			if (isFim(linha[numEntrada]) == false)
+			{
+				combinar(linha[numEntrada]);
+			}
+		} while (isFim(linha[numEntrada++]) == false);
+		numEntrada--;
+	}
 }
